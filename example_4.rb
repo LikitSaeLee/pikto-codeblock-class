cfcf0ec8ad4f549b352099b210649315ea387438
LikitSaeLee <likit.usm@gmail.com>
2016-07-17 19:54:03 +0800

    checked example 4

--- example_4.rb
+++ example_4.rb
@@ -1,17 +1,20 @@
+# Topic: DIfferent between Lambda and Proc
+#
 # The first different is how they return
 def echo( &func )
   func.call( 2, 4 )
-  puts 'Lambda return at this line' # return nil
+  return 'Lambda return at this line'
 end
 
 lambda = lambda { |a,b| a + b }
 proc = proc.new { |a,b| a + b }
 
-# print 'Lambda return at this line' and return nil
-# because puts will return nil.
-# after lambda is called, the echo function
-# continue to execute
+# return 'Lambda return at this line'
+# this is because after lambda function is called, the echo function
+# continue to execute and return the message
 echo( lambda )
 
 # return 6, without printing the message
+# because the return keyword for proc function
+# with interrupt its caller function.
 echo( proc )
