d4a33e1b97f7d430ac6fe88b3d294c4deebccc23
LikitSaeLee <likit.usm@gmail.com>
2016-07-17 19:56:37 +0800

    checked example 5

--- example_5.rb
+++ example_5.rb
@@ -1,12 +1,13 @@
+# Topic: Different between Lambda and Proc
 lambda_obj = lambda { |a,b| a + b  }
 proc_obj = proc.new { |a,b| a + b  }
 
-# the second different is how they consider the argument
-
+# the second different is how they treat the argument
 # you can call lambda with any number of argument
 # return 3
 lambda_obj.call( 1, 2, 3, 4 )
 
 # do this in proc will return in error
+# because we didn't define the function to accept more than 2 arguments.
 proc_obj.call( 1, 2, 3, 4 )
 
