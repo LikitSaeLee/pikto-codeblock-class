28c06d5b360b61f822c51750640b9d24d98994bc
LikitSaeLee <likit.usm@gmail.com>
2016-07-17 20:06:26 +0800

    bump 6c

--- example_6c.rb
+++ example_6c.rb
@@ -1,3 +1,17 @@
+# Topic: Public and Private Interface
+# Public interface of a class is the method that can be called
+# externally for a class.
+
+# Private interface is the method that is being used by the class
+# internally only and should not be exposed externally.
+# For example.
+
+triangle = Triangle.new( 1, 2, 3, 4 )
+# this will raise error because we are called the private method of the class.
+triangle.area
+
+
+
 class Triangle
   attr_reader :length, :height, :width
 
